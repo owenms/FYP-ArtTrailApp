b5080611bc0c06bb1dd24434d60cd5841a68ee00
Owen <111442392@umail.ucc.ie>
2015-01-09 22:37:42 +0000

    Another init commit. Android Maps API v2 support added

--- app/src/main/java/ie/ucc/cs1/ojms1/arttrail/MainActivity.java
+++ app/src/main/java/ie/ucc/cs1/ojms1/arttrail/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.ActionBar;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -16,6 +17,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 
 
@@ -40,6 +42,14 @@ public class MainActivity extends Activity
         mNavigationDrawerFragment = (NavigationDrawerFragment)
                 getFragmentManager().findFragmentById(R.id.navigation_drawer);
         mTitle = getTitle();
+        Button mapButton = (Button) findViewById(R.id.mapButton);
+        mapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mapActivity = new Intent(MainActivity.this, MapActivity.class);
+                startActivity(mapActivity);
+            }
+        });
 
         // Set up the drawer.
         mNavigationDrawerFragment.setUp(
